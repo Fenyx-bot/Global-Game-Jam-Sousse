936e4646a822355686a9a2001b7f4478a93d1435
Hudie320 <ahmed.guemri00@gmail.com>
2023-02-03 19:27:02 +0100

    shooting enemy and enemy movement

--- Scripts/ShootingEnemy.gd
+++ Scripts/ShootingEnemy.gd
@@ -2,17 +2,24 @@ extends KinematicBody2D
 
 export var chase_speed = 200
 
-onready var AimRay = $AimRay
+const gravity = 90
+
+onready var DetectionRay = $DetectionRay
 
 var target = null
+var velocity = Vector2.ZERO
 
 func _physics_process(delta):
+	if !is_on_floor():
+		velocity.y += gravity
+		if velocity.y > 200:
+			velocity.y = 200
 	if target:
 		var target_dir = Vector2(target.global_position - global_position).normalized()
-		AimRay.cast_to = target_dir * 128
-		if AimRay.is_colliding() and AimRay.get_collider().is_in_group('player'):
-			move_and_slide(target_dir * chase_speed)
-
+		DetectionRay.cast_to = target_dir * 128
+		if DetectionRay.is_colliding() and DetectionRay.get_collider().is_in_group('player'):
+			move_and_slide(target_dir.project(Vector2.RIGHT) * chase_speed)
+	velocity = move_and_slide(velocity, Vector2.UP)
 func _on_DetectionArea_body_entered(body):
 	if body.is_in_group('player'):
 		target = body
@@ -21,3 +28,21 @@ func _on_DetectionArea_body_entered(body):
 func _on_DetectionArea_body_exited(body):
 	if body.is_in_group('body'):
 		target = null
+
+
+func _on_ShootArea_body_entered(body):
+	if body.is_in_group('player'):
+		shoot()
+		$ShootTimer.start()
+
+
+func _on_ShootArea_body_exited(body):
+	if body.is_in_group('player'):
+		$ShootTimer.stop()
+
+
+func _on_ShootTimer_timeout():
+	shoot()
+	
+func shoot():
+	print('Shoot')
