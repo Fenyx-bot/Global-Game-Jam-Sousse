230cc64605e0c619fa36b2438f896267509ed342
ahmed <ahmed.guemri00@gmail.com>
2023-02-04 15:50:19 +0100

    music fix and main menu

---
+++ Scripts/MenuScene.gd
@@ -0,0 +1,70 @@
+extends Control
+
+var selected_button = 0
+var play_pressed = false
+
+func _ready():
+	$FadeOut/FadeOutAnim.play_backwards("fade_out")
+
+
+func _input(event):
+	if event.is_action_pressed("ui_down"):
+		selected_button = min(selected_button + 1, 2)
+		select_button()
+	if event.is_action_pressed("ui_up"):
+		selected_button = max(selected_button - 1, 0)
+		select_button()
+	if event.is_action_pressed("ui_accept"):
+		match selected_button:
+			0:
+				_on_PlayButton_pressed()
+			1:
+				_on_SettingsButton_pressed()
+			2:
+				_on_Quit_pressed()
+	
+func select_button():
+	for btn in $BG/ElementsContainer/Buttons/ButtonsContainer.get_children():
+		btn.get_node("Label").hide()
+	var btn = $BG/ElementsContainer/Buttons/ButtonsContainer.get_child(selected_button)
+	btn.get_node("Label").show()
+	
+
+
+func _on_PlayButton_pressed():
+	$BGMAudio.stop()
+	play_pressed = true
+	$FadeOut/FadeOutAnim.play("fade_out")
+
+
+
+func _on_SettingsButton_pressed():
+	$SettingsAnim.play("switch")
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
+
+
+func _on_FadeOutAnim_animation_finished(anim_name):
+	if play_pressed:
+		get_tree().change_scene("res://Scenes/World.tscn")
+
+
+func _on_BackButton_pressed():
+	$SettingsAnim.play_backwards("switch")
+
+
+func _on_FullScreenButton_pressed():
+	var FullscreenButton = $BG/ElementsContainer/SettingsRect/ButtonsContainer/FullScreenButton
+	FullscreenButton.text = "Fullscreen Mode: ON" if FullscreenButton.text == "Fullscreen Mode: OFF" else "Fullscreen Mode: ON"
+	OS.window_fullscreen = !OS.window_fullscreen
+
+
+func _on_VolumeSlider_drag_ended(value_changed):
+	var VolumeSlider = $BG/ElementsContainer/SettingsRect/ButtonsContainer/VolumeSlider
+	AudioServer.set_bus_volume_db(0, VolumeSlider.value)
+
+func _on_MusicSlider_drag_ended(value_changed):
+	var MusicSlider = $BG/ElementsContainer/SettingsRect/ButtonsContainer/MusicSlider
+	AudioServer.set_bus_volume_db(1, MusicSlider.value)
