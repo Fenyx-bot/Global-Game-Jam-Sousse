645b3a9c7b1e53e97e73745ba69dc0ee7211faaf
ahmed <ahmed.guemri00@gmail.com>
2023-02-05 06:48:45 +0100

    boss initial code

---
+++ Scripts/Boss.gd
@@ -0,0 +1,62 @@
+extends KinematicBody2D
+
+onready var BossBar = get_node("/root/World/HUD/Control/BossBar")
+
+var bullet = preload("res://Nodes/Bullet.tscn")
+
+const move_speed = 200
+
+var Health = 500
+
+var low_hp = false
+var move_dir = Vector2.LEFT
+var velocity = Vector2.ZERO
+var target = null
+var isDead = false
+
+func _ready():
+	BossBar.max_value = Health
+
+func _process(delta):
+	BossBar.value = Health
+
+func _physics_process(delta):
+	velocity = move_and_slide(move_dir * move_speed)
+
+func _on_MoveTimer_timeout():
+	move_dir *= -1
+
+
+func _on_DetectionArea_body_entered(body):
+	if body.is_in_group('player'):
+		target = body
+
+func _on_DetectionArea_body_exited(body):
+	if body.is_in_group('player'):
+		target = null
+
+func _on_ShootTimer_timeout():
+	if target:
+		shoot()
+
+
+func shoot():
+	#Instatiates a bullet and shoot its toward the mouse
+	var bulletInstance = bullet.instance()
+	bulletInstance.dir = (target.global_position - global_position).normalized()
+	bulletInstance.global_position = global_position + Vector2(0,64)
+	bulletInstance.rotation = get_angle_to(target.global_position)
+	get_tree().get_root().add_child(bulletInstance)
+	
+func TakeDamage(arg):
+	Health -= arg
+	if Health <= 0:
+		Die()
+
+func Die():
+	isDead = true
+	velocity = Vector2.ZERO
+	get_node("CollisionShape2D").set_deferred("disabled", true)
+	# GAME OVER
+	OS.alert('You win!')
+	
